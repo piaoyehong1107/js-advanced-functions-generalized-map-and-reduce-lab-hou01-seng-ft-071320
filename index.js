d739474f980c0ed11191cfb3629865a663f16fc2
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-08 05:20:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(src, cb) {
+  let arr = []
+  for (let i = 0; i < src.length; i++) {
+    arr.push(cb(src[i]))
+  }
+  return arr;
+}
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
